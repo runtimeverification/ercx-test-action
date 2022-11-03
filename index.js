b34ff2719bf236e4e72f4c9217f391b92f2a1cf3
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2022-11-03 14:01:07 +0100

    Don't fail on non-zero status code

--- index.js
+++ index.js
@@ -7,23 +7,16 @@ const { promises: fs } = require('fs');
 async function run() {
   try {
     core.info('Running golden tests');
-    let output = '';
-    let error = '';
-    const options = {
-      listeners : {
-        stdout: (data) => {
-          output += data.toString();
-        },
-        stderr: (data) => {
-          error += data.toString();
-        }
-      }
-    };
-    await exec.exec('forge', ['test', '--silent', '--match-contract', 'ERC20MockTest'], options);
+    
+    const output = await exec.getExecOutput(
+      'forge',
+      ['test', '--silent', '--match-contract', 'ERC20MockTest'],
+      {ignoreReturnCode: true}
+    );
 
     const expected = await fs.readFile('/test/', 'utf8');
 
-    if (expected !== output) {
+    if (expected !== output.stdout) {
       core.setFailed("foundry output did not match expected output.");
     }
 
