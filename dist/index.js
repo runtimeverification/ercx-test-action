bac714d987a697e59a04ddef17d0fbbe22b0e2c8
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2022-11-04 10:52:32 +0100

    Compare golden files with actual output

--- dist/index.js
+++ dist/index.js
@@ -4123,16 +4123,86 @@ async function run() {
       ['test', '--silent', '--match-contract', 'ERC20MockTest'],
       {ignoreReturnCode: true}
     );
-
+    
+    const actual = output.stdout;
     const expected = await fs.readFile('test/golden/ERC20MockTest.t.sol.out', 'utf8');
 
-    if (expected !== output.stdout) {
+    if (!compare(actual, expected)) {
       core.setFailed("foundry output did not match expected output.");
     }
 
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function compare(actual, expected) {
+  let actualLines = actual.split("\n");
+  let expectedLines = expected.split("\n");
+  if (actualLines.length !== expectedLines.length) {
+    return false;
+  }
+  // Remove the first and last line from the output. These lines
+  // contain metadata and not test cases
+  actualLines = actualLines.slice(2, -2);
+  expectedLines = expectedLines.slice(2, -2);
+  // Construct a mapping from function signatures to success states
+  // for each test case of actual foundry output
+  const testResults = new Map();
+  for (let line of actualLines) {
+    const testResult = parseLine(line);
+    testResults.set(testResult.signature, testResult.result);
+  }
+  // Go over each test case of the expected foundry ouput and check
+  // if it matches the actual output. If not, we shortcut and return
+  // false
+  for (let line of expectedLines) {
+    const testResult = parseLine(line);
+    if (testResults.get(testResult.signature) !== testResult.result) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function parseLine(line) {
+  // Strip color codes from input line
+  line = line.split(/\x1B\[\d+m/).join();
+  let result = "";
+  let signature = "";
+  let brackets = 0;
+  let state = "before result";
+  for (let char of line) {
+    if (state === "before result") {
+      if (char === "[") {
+        brackets++;
+        state = "inside result"
+      } 
+    } else if (state == "inside result") {
+      if (char === "[") {
+        brackets++;
+      } else if (char === "]") {
+        brackets--;
+        if (brackets === 0) {
+          state = "before signature"
+        }
+      } else {
+        result += char;
+      }
+    } else if (state == "before signature") {
+      if (char.match(/\w/)) {
+        state = "inside signature"
+        signature += char;
+      }
+    } else if (state == "inside signature") {
+      if (char.match(" ")) {
+        state = "after signature";
+      } else {
+        signature += char;
+      }
+    }
+  }
+  return { result, signature }
 }
 
 run();
